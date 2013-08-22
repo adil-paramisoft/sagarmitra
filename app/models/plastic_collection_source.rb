6099586292fc6c9fa1445bb303a3d31420c8692e
Atul Veer <atul@paramisoft.com>
2013-08-23 01:42:55 +0530

    SAG-3 #comment added validations for plastic collection sources, fixed typo

--- app/models/plastic_collection_source.rb
+++ app/models/plastic_collection_source.rb
@@ -1,3 +1,6 @@
 class PlasticCollectionSource < ActiveRecord::Base
   belongs_to :plastic_collection_event
+  
+  validates :name , :address , :contact_person, :phone_no_1, :presence => true
+  
 end
