75ab7b0131e40f9a94cde9385aa33be62753527d
Atul Veer <atul@paramisoft.com>
2013-09-06 02:26:23 +0530

    SAG-3 #comment added number of collections to schools

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -71,6 +71,6 @@ class SchoolsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def school_params
-      params.require(:school).permit(:name, :address, :school_type_id, :program_state_id, :total_students, :school_medium_id)
+      params.require(:school).permit(:name, :address, :school_type_id, :program_state_id, :total_students, :school_medium_id, :number_of_collections)
     end
 end
---
+++ db/migrate/20130905204758_add_numner_of_collections_to_schools.rb
@@ -0,0 +1,5 @@
+class AddNumnerOfCollectionsToSchools < ActiveRecord::Migration
+  def change
+    add_column :schools , :number_of_collections , :integer
+  end
+end
