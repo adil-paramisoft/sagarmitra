d56a6c59f56e9fdf1076951822ae488ce13700e9
AniruddhaBirajdar <aniruddha@paramisoft.com>
2013-11-29 16:24:33 +0530

    seed files correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ puts " -- Creating roles"
 
 Role.create([{name: 'admin', description: 'Super admin of the app'},
              {name: 'school-admin', description: 'Admin for school'},
-             {name: 'school-volunteer'}
+             {name: 'school-volunteer'},
              {name: 'program-volunteer'}])
 puts " -- Done!"
 puts " -- Creating Program States"
