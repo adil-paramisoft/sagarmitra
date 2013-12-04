a060c1b73ba7afe73972d8b686076f9d00a9613b
AniruddhaBirajdar <aniruddha@paramisoft.com>
2013-12-04 16:51:35 +0530

    remove date from modwl

--- app/models/plastic_collection_event.rb
+++ app/models/plastic_collection_event.rb
@@ -2,7 +2,7 @@ class PlasticCollectionEvent < ActiveRecord::Base
   has_event_calendar
   belongs_to :school
   belongs_to :plastic_collection_source
-  validates :school_id , :date , :plastic_collection_source_id, :presence => true
+  validates :school_id  , :plastic_collection_source_id, :presence => true
   
   def name
     self.school.name
