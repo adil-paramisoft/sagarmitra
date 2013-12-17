7573168e1f43e434f52ad2c57b44c85fbcd59f66
AniruddhaBirajdar <aniruddha@paramisoft.com>
2013-12-17 14:04:22 +0530

    SAG-38 #validated new plastic collection sorce

--- app/models/plastic_collection_source.rb
+++ app/models/plastic_collection_source.rb
@@ -1,7 +1,12 @@
 class PlasticCollectionSource < ActiveRecord::Base
   belongs_to :plastic_collection_event
   #validations
-  validates :name , :address , :contact_person, :phone_no_1, 
+  validates :name , :address , :contact_person,:phone_no_1, 
             :presence => true
-  
+            
+  validates :phone_no_1, format: { with: /\d{10}/, message: "bad format" }
+  validates :phone_no_2, format: { with: /\d{10}/, message: "bad format" },
+            allow_blank: true
+  validates :phone_no_1,:phone_no_2,
+            :numericality => {:greater_than_or_equal_to => 0},allow_blank: true
 end
