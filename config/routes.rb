a17e2fa8e9292d2834f374fec8cc065aed14dde4
Aniruddha Birajdar <aniruddha@paramisoft.com>
2013-12-23 13:33:54 +0530

    SAG-50 #added  New Plastic collection source in admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -105,6 +105,7 @@ Sagarmitra::Application.routes.draw do
 namespace :admin do
   get '/index' => 'program#index'
     resources :plastic_collection_events
+    resources :plastic_collection_sources
 end
 
 
