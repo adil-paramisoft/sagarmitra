a3673ab0ba629f1c21a892fc5af44b98e66d65fb
aniruddha <aniruddha@paramisoft.com>
2014-04-02 15:32:23 +0530

    SAG-69 #principal_detail.photo.flickr_url

--- app/models/school_volunteer.rb
+++ app/models/school_volunteer.rb
@@ -11,15 +11,15 @@ class SchoolVolunteer < ActiveRecord::Base
    phony_normalize :mobile, :default_country_code => 'IN'
    
    # Creates method normalized_fax_number that returns the normalized version of fax_number
-     phony_normalized_method :mobile
+   #  phony_normalized_method :mobile
   #validates :mobile, format: { with: /\d{10}/, message: "bad format" }
   validates_plausible_phone :mobile, :with => /\d{10}/
   validates :mobile, :phony_plausible => true
   validates_uniqueness_of :email
   # Normalizes the attribute itself before validation
-  phony_normalize :mobile, :default_country_code => 'IN'
+  #phony_normalize :mobile, :default_country_code => 'IN'
   # Creates method normalized_fax_number that returns the normalized version of fax_number
-  phony_normalized_method :mobile
+  #phony_normalized_method :mobile
 
   # Nested attributes
   accepts_nested_attributes_for :school
@@ -28,6 +28,8 @@ class SchoolVolunteer < ActiveRecord::Base
   def upload_flickr_photos
     self.school.upload_flickr_photo
     self.school.principal_detail.upload_flickr_photo
+    p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+    p self.school.principal_detail.photo.flickr_url
   end
   
 end
